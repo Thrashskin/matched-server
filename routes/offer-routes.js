81445df4aa5c36dd546d9782808049716dd94715
Thrashskin <jmontegonz@gmail.com>
2021-03-02 23:31:17 +0100

    Added routes to retrieve applications and saved offers

--- routes/offer-routes.js
+++ routes/offer-routes.js
@@ -4,51 +4,85 @@ const Seeker = require('../models/Seeker');
 const offerRoutes = express.Router();
 const Offer = require('../models/Offer');
 const mongoose = require('mongoose');
-const {User} = require('../models/User');
+const { User } = require('../models/User');
 
 //GET offer details
 offerRoutes.get('/offer/:offerID', (req, res, next) => {
-  const {offerID} = req.params
+  const { offerID } = req.params
 
   Offer.findById(offerID).
-  then(offerFromDB => {
-    res.status(200).json(offerFromDB);
-  })
-  .catch(error => res.json(error));
+    then(offerFromDB => {
+      res.status(200).json(offerFromDB);
+    })
+    .catch(error => res.json(error));
 });
 
 //GET all offers
-offerRoutes.get('/:companyID/offers', (req,res,next) => {
+offerRoutes.get('/:companyID/offers', (req, res, next) => {
 
   let companyID = req.params.companyID
 
-  Offer.find( {publisher: companyID} )
-  .then(offersFromDB => res.status(200).json(offersFromDB))
-  .catch(error => res.status(400).json(error));
+  Offer.find({ publisher: companyID })
+    .then(offersFromDB => res.status(200).json(offersFromDB))
+    .catch(error => res.status(400).json(error));
+
+})
+
+//GET applied offers
+offerRoutes.get('/:seekerID/applications', (req, res, next) => {
+
+  let seekerID = req.params.seekerID
+
+  // Offer.find({ publisher: companyID })
+  //   .then(offersFromDB => res.status(200).json(offersFromDB))
+  //   .catch(error => res.status(400).json(error));
+
+  Seeker.findById(seekerID)
+    .populate('offers')
+    .then(userFromDB => {
+      console.log(userFromDB.offers)
+      res.status(200).json(userFromDB.offers)
+    })
+    .catch(error => res.status(400).json(error));
+
+
+
+
+})
+
+//GET saved offers
+offerRoutes.get('/:seekerID/saved', (req, res, next) => {
+
+  let seekerID = req.params.seekerID
+
+  Seeker.findById(seekerID)
+    .populate('saved')
+    .then(userFromDB => res.status(200).json(userFromDB.saved))
+    .catch(error => res.status(400).json(error));
 
 })
 
 //POST new offer
 
-offerRoutes.post('/offer', (req, res, next) => { 
+offerRoutes.post('/offer', (req, res, next) => {
 
   var publisher = req.session.passport.user;
-  var newOffer = new Offer({publisher,...req.body})
+  var newOffer = new Offer({ publisher, ...req.body })
 
   Offer.create(newOffer)
-  .then(createdOffer => {
-    console.log('new offer id: ', createdOffer._id)
-    console.log('publisher: ', createdOffer.publisher)
-    return Company.findByIdAndUpdate(createdOffer.publisher, {
-      $push: { offers: createdOffer._id }
+    .then(createdOffer => {
+      console.log('new offer id: ', createdOffer._id)
+      console.log('publisher: ', createdOffer.publisher)
+      return Company.findByIdAndUpdate(createdOffer.publisher, {
+        $push: { offers: createdOffer._id }
+      })
+        .then(response => res.json(response))
+        .catch(error => res.status(409).json(error)); //catch-companyFindByIdAndUpdate
     })
-    .then(response => res.json(response))
-    .catch(error => res.status(409).json(error)); //catch-companyFindByIdAndUpdate
-  })
-  .catch(error => {
-    console.log(error)
-    res.status(408).json(error)
-  });//catch-Offer.create
+    .catch(error => {
+      console.log(error)
+      res.status(408).json(error)
+    });//catch-Offer.create
 
 
 
@@ -64,23 +98,23 @@ offerRoutes.put('/offer/:offerID', (req, res, next) => {
   }
 
   Offer.findByIdAndUpdate(req.params.offerID, req.body)
-  .then(() => {
-    res.json({ message: `Offer ${req.params.offerID} updated` });
-  })
-  .catch(error => res.json(error))
+    .then(() => {
+      res.json({ message: `Offer ${req.params.offerID} updated` });
+    })
+    .catch(error => res.json(error))
 });
 
 //DELETE offer
 
 offerRoutes.delete('/offer/:offerID', (req, res, next) => {
-  
+
   if (!mongoose.Types.ObjectId.isValid(req.params.offerID)) {
     res.status(400).json({ message: 'Specified id is not valid' });
     return;
   }
 
-  let {user} = req.session.passport;
-  let {offerID} = req.params
+  let { user } = req.session.passport;
+  let { offerID } = req.params
 
 
 
@@ -94,20 +128,20 @@ offerRoutes.delete('/offer/:offerID', (req, res, next) => {
 
 
   Offer.findByIdAndRemove(offerID)
-  .then(() => {
+    .then(() => {
 
-    User.findByIdAndUpdate(user, {
-      $pull: {offers: offerID}
+      User.findByIdAndUpdate(user, {
+        $pull: { offers: offerID }
+      })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+
+      res.json({ message: `Offer ${req.params.id} deleted.` });
+
     })
-    .then(response => console.log(response))
-    .catch(error => console.log(error))
-
-    res.json({ message: `Offer ${req.params.id} deleted.` });
-
-  })
-  .catch(error => {
-    res.json(error);
-  });
+    .catch(error => {
+      res.json(error);
+    });
 
 
   //res.json({ message: `Offer ${req.params.id} deleted.` });
@@ -124,60 +158,88 @@ offerRoutes.put('/offer/:offerID/apply', (req, res, next) => {
   }
 
   var applicant = req.session.passport.user;
-  var {offerID} = req.params;
+  var { offerID } = req.params;
 
   //Check that the User is a Seeker
 
   User.findById(applicant)
-  .then(userFromDB => {
-    if (userFromDB.offers.includes(offerID)) {
-      res.status(405).json({message: 'User is already in the list of candidates for this offer'});
-      return;
-    }
+    .then(userFromDB => {
+      if (userFromDB.offers.includes(offerID)) {
+        res.status(405).json({ message: 'User is already in the list of candidates for this offer' });
+        return;
+      }
       //Push the offerID into the appliedOffers array of the Seeker
-    User.findByIdAndUpdate(applicant, {
-      $push: {offers: offerID}
-    })
-    .then(response => {
-      Offer.findByIdAndUpdate(offerID, {
-        $push: {candidates: applicant}
+      User.findByIdAndUpdate(applicant, {
+        $push: { offers: offerID }
       })
-      .then(() => console.log('Succesfully applied to offer'))
-      .catch(error => res.json(error))
-      res.status(200).json(response)
+        .then(response => {
+          Seeker.findByIdAndUpdate(applicant, { //We use Seeker instead of User so we can $push and $pull in the same operation.
+            $pull: { saved: offerID }
+          })
+            .then(response => {
+              Offer.findByIdAndUpdate(offerID, {
+                $push: { candidates: applicant }
+              })
+                .then(() => console.log('Succesfully applied to offer'))
+                .catch(error => res.json(error))
+            })
+            .catch(error => res.json(error))
+          res.status(200).json(response)
+        })
+        .catch(theError => res.json(theError))
     })
-    .catch(theError => res.json(theError))
-  })
-  .catch(error => res.json(error));
+    .catch(error => res.json(error));
+
+  // User.findById(applicant)
+  //   .then(userFromDB => {
+  //     if (userFromDB.offers.includes(offerID)) {
+  //       res.status(405).json({ message: 'User is already in the list of candidates for this offer' });
+  //       return;
+  //     }
+  //     //Push the offerID into the appliedOffers array of the Seeker
+  //     User.findByIdAndUpdate(applicant, {
+  //       $push: { offers: offerID }
+  //     })
+  //       .then(response => {
+  //         Offer.findByIdAndUpdate(offerID, {
+  //           $push: { candidates: applicant }
+  //         })
+  //           .then(() => console.log('Succesfully applied to offer'))
+  //           .catch(error => res.json(error))
+  //         res.status(200).json(response)
+  //       })
+  //       .catch(theError => res.json(theError))
+  //   })
+  //   .catch(error => res.json(error));
 });
 
 //SAVE OFFER
 
 offerRoutes.put('/offer/:offerID/save', (req, res, next) => {
 
-    if (!mongoose.Types.ObjectId.isValid(req.params.offerID)) {
-      res.status(400).json({ message: 'Specified id is not valid' });
-      return;
-    }
+  if (!mongoose.Types.ObjectId.isValid(req.params.offerID)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
 
-    var userID = req.session.passport.user;
-    var {offerID} = req.params
+  var userID = req.session.passport.user;
+  var { offerID } = req.params
 
-    User.findById(userID)
+  User.findById(userID)
     .then(userFromDB => {
 
       if (userFromDB.saved.includes(offerID)) {
-        res.status(406).json({message: 'You already saved this offer'});
+        res.status(406).json({ message: 'You already saved this offer' });
         return;
       }
 
       Seeker.findByIdAndUpdate(userID, {
-        $push:{saved: offerID}
+        $push: { saved: offerID }
       })
-      .then(response => res.status(200).json(response)) //findAndUpdate
-      .catch(error => res.status(400).json(error));//findAndUpdate
-      }) // findById
-      .catch(error => res.status(400).json(error)) // findById
+        .then(response => res.status(200).json(response)) //findAndUpdate
+        .catch(error => res.status(400).json(error));//findAndUpdate
+    }) // findById
+    .catch(error => res.status(400).json(error)) // findById
 
 });
 
